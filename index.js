4f7308b4f0a8f5fc56491f1dc32fd172ecb37634
Aron Galvan <arongalvan03@gmail.com>
2022-04-17 14:57:45 -0700

    commit tree 4314997942941273bd2f26acec0f1797b5d522fe

---
+++ index.js
@@ -0,0 +1,14 @@
+const introduction = (name) => 
+{
+    return `Hi, my name is ${name}.`;
+}
+
+const introductionWithLanguage = (name, language) => 
+{
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+const introductionWithLanguageOptional = (name = "Gracie", language = "JavaScript") => 
+{
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
